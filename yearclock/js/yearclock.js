6e21ac5bc3d0b24d3a940d6f99d442f3618be826
Paul Dale <ldpercy@gmail.com>
2025-03-18 18:49:12 +1100

    Relocate setup function

--- yearclock/js/yearclock.js
+++ yearclock/js/yearclock.js
@@ -3,48 +3,6 @@
 
 
 
-/* setup
-*/
-function setup() {
-	// Language
-	config.userLang = navigator.language || navigator.userLanguage;
-
-	config.languageParam = superLang( getParameterByName('language') );
-	config.browserLanguage = superLang( navigator.language || navigator.userLanguage );
-
-	config.monthNames = i18n.gregLocal[config.languageParam] || i18n.gregLocal[config.browserLanguage] || i18n.gregLocal["en"]
-
-	// Set Current Date
-	const dateParam = getParameterByName('date');
-	config.now = dateParam ? new Date(dateParam) : new Date();
-	config.year = config.now.getFullYear();
-
-	// Set Up Months
-	config.months = config.monthNames.map(function( monthName, monthNumber )
-	{
-		const startDate = new Date(config.year, monthNumber)
-		const nextMonth = new Date(config.year, monthNumber + 1)
-		const endDate   = new Date(nextMonth - 1000)
-
-		return { "name": monthName, "code": config.monthCodes[monthNumber], "startDate": startDate, "endDate": endDate }
-	})
-
-	// Set Up Days
-	for (let date = new Date(config.year,0); date.getFullYear() <= config.year; d = incrementDay(date))
-	{
-		const day = {
-			date:    new Date(date),
-			first:   date.getDate() == 1,
-			weekend: isWeekend(date)
-		}
-
-		config.days.push(day)
-	}
-} // setup
-
-
-
-
 /* Draw Clock
 */
 function drawClock()
