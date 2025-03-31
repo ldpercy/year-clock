21045dcf1a72386bfa94e7360e58104f305634f7
Paul Dale <ldpercy@gmail.com>
2025-03-31 11:31:36 +1100

    Convert needle; also fixed the drop-shadow

--- yearclock/theme/plain-svg/yearclock.js
+++ yearclock/theme/plain-svg/yearclock.js
@@ -2,7 +2,7 @@
 // Plain SVG
 //
 
-theme.clock.clockRadius  = 1180,
+theme.clock.clockRadius  = 1170,
 theme.clock.needleLength = 950;
 
 
@@ -17,7 +17,25 @@ theme.clock.drawYear = function() {
 	const x = theme.clock.innerRadius * 0.55 * labelSide;
 	const y = 0;
 
-	const element = `<text x="${x}" y="${y}" class="label year">${config.year}</text>`;
+	const svg = `<text x="${x}" y="${y}" class="label year">${config.year}</text>`;
 
-	theme.clock.element.innerHTML += element;
+	theme.clock.element.innerHTML += svg;
+}
+
+
+theme.clock.drawNeedle = function() {
+	const path = `
+		M 12 160
+		L -12 160
+		L 0, -${theme.clock.needleLength}
+		Z
+		M 30 0
+		A 30,30 0 1 1 -30,00
+		A 30,30 0 1 1 30,00`;
+	const transform = svgRotateString(dateDegrees(config.date),0,0);
+	const svg = `
+		<g class="needle">
+			<path d="${path}" transform="${transform}"></path>
+		</g>`;
+	theme.clock.element.innerHTML += svg;
 }
