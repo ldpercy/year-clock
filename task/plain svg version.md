b7838d4195bd4f5dd41760e86ab23ea4ca781e11
Paul Dale <ldpercy@gmail.com>
2025-03-31 11:09:46 +1100

    Convert drawYear

--- task/plain svg version.md
+++ task/plain svg version.md
@@ -5,3 +5,9 @@ I'm going to have a crack at rewriting the current snap-svg code with plain SVG 
 
 Some parts I think will be fairly easy, others not so sure about.
 
+
+
+Plain SVG Theme
+---------------
+I've created a new theme 'plain-svg', will begin converting some of the draw functions over.
+
--- yearclock/theme/plain-svg/yearclock.js
+++ yearclock/theme/plain-svg/yearclock.js
@@ -4,3 +4,20 @@
 
 theme.clock.clockRadius  = 1180,
 theme.clock.needleLength = 950;
+
+
+theme.clock.element = document.getElementById('clock');
+
+
+
+theme.clock.drawYear = function() {
+	// Year Label
+	const yearOnLeft = dateRatio(config.date) < 0.5
+	const labelSide = yearOnLeft ? -1 : 1
+	const x = theme.clock.innerRadius * 0.55 * labelSide;
+	const y = 0;
+
+	const element = `<text x="${x}" y="${y}" class="label year">${config.year}</text>`;
+
+	theme.clock.element.innerHTML += element;
+}
