80594ffa8d73c4ebaf9b7253cbeca861b66b142c
Paul Dale <ldpercy@gmail.com>
2025-03-31 13:02:30 +1100

    Convert month sectors and labels
    There's still a Snap.deg lurking in there though

--- yearclock/theme/plain-svg/yearclock.js
+++ yearclock/theme/plain-svg/yearclock.js
@@ -2,9 +2,11 @@
 // Plain SVG
 //
 
-theme.clock.clockRadius  = 1170,
-theme.clock.needleLength = 950;
-theme.clock.innerRadius  = 920;
+theme.clock.clockRadius      = 1170,
+theme.clock.needleLength     = 950;
+theme.clock.innerRadius      = 920;
+theme.clock.outerRadius      = 1120;
+theme.clock.monthLabelRadius = 980;
 
 
 
@@ -16,7 +18,7 @@ theme.clock.drawClock = function()
 	theme.clock.element = document.getElementById('clock');
 
 	theme.clock.drawFace();
-	//theme.clock.drawMonths();
+	theme.clock.drawMonths();
 	//theme.clock.drawDayTicks();
 	theme.clock.drawYear();
 	theme.clock.drawNeedle();
@@ -29,6 +31,36 @@ theme.clock.drawFace = function() {
 	const svg = `<circle cx="0" cy="0" r="${theme.clock.clockRadius}" class="face"></circle>`
 	theme.clock.element.innerHTML += svg;
 }
+
+
+theme.clock.drawMonths = function() {
+	// Draw Months
+	for (let month of config.months)
+	{
+		const startAngle = dateRadians(month.startDate);
+		const endAngle   = dateRadians(month.endDate);
+
+		// Month sector
+		const sectorPath = sector(startAngle, endAngle, theme.clock.innerRadius, theme.clock.outerRadius );
+		sectorSvg = `<path d="${sectorPath}" class="sector month ${month.code}"></path>`;
+		theme.clock.element.innerHTML += sectorSvg;
+
+		// Month Label
+		const midAngle = midpoint(startAngle,endAngle) + (Math.TAU * 0.25);
+		const upsideDown = Math.cos(midAngle) < 0;
+
+		const yOffset        = upsideDown ? theme.clock.monthLabelRadius : 0 - theme.clock.monthLabelRadius;
+		const labelAngle     = upsideDown ? midAngle + Math.PI          : midAngle;
+		const labelTransform = svgRotateString(Snap.deg(labelAngle),0,0);;
+
+		const labelSvg = `<text x="0" y="${yOffset}" class="label month" transform="${labelTransform}">${month.name}</text>`;
+		theme.clock.element.innerHTML += labelSvg;
+	}
+}/* drawMonths */
+
+
+
+
 
 
 theme.clock.drawYear = function() {
