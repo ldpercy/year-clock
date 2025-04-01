855e7b1594058fd384a532aeada3e425267d60f0
Paul Dale <ldpercy@gmail.com>
2025-04-01 15:27:08 +1100

    Add a couple of basic styles to the common theme; update the test theme so it works

--- yearclock/theme/test/yearclock.js
+++ yearclock/theme/test/yearclock.js
@@ -4,3 +4,8 @@
 
 theme.clock.clockRadius  = 1180,
 theme.clock.needleLength = 950;
+theme.clock.innerRadius       = 920;
+theme.clock.outerRadius       = 1120;
+theme.clock.monthLabelRadius  = 980;
+theme.clock.weekdayTickLength = 40;
+theme.clock.weekendTickLength = 55;
