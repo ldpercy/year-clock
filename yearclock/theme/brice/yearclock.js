caaad3e893298eeb19b688fe0bc364c3993be5cf
Paul Dale <ldpercy@gmail.com>
2025-04-01 14:48:32 +1100

    Port in changes from plain-svg, with a few adjustments

--- yearclock/theme/brice/yearclock.js
+++ yearclock/theme/brice/yearclock.js
@@ -21,10 +21,11 @@ theme.clock = {
 theme.clock.drawClock = function()
 {
 	// Set Up Drawing
-	theme.clock.drawing = Snap("#clock");
+	theme.clock.element = document.getElementById('clock');
 
 	theme.clock.drawFace();
-	theme.clock.drawMonths();
+	theme.clock.drawMonthSectors();
+	theme.clock.drawMonthLabels();
 	theme.clock.drawDayTicks();
 	theme.clock.drawYear();
 	theme.clock.drawNeedle();
@@ -32,74 +33,83 @@ theme.clock.drawClock = function()
 }/* drawClock */
 
 
+
 theme.clock.drawFace = function() {
-	theme.clock.drawing.circle(0, 0, theme.clock.clockRadius)
-		.addClass('face year');
+	const svg = `<circle cx="0" cy="0" r="${theme.clock.clockRadius}" class="face"></circle>`
+	theme.clock.element.innerHTML += svg;
 }
 
 
-theme.clock.drawMonths = function() {
-	// Draw Months
+theme.clock.drawMonthSectors = function() {
+	let newSvg = '';
 	for (let month of config.months)
 	{
-		const startAngle = dateRadians(month.startDate)
-		const endAngle   = dateRadians(month.endDate)
-
-		// Month sector
-		const sectorPath = sector(startAngle, endAngle, theme.clock.innerRadius, theme.clock.outerRadius );
-		theme.clock.drawing.path(sectorPath)
-			.addClass("sector month")
-			.addClass(month.code)
-
-		// Month Label
-		const midAngle = midpoint(startAngle,endAngle);
-		const upsideDown = Math.cos(midAngle) < 0
-
-		const yOffset    = upsideDown ? theme.clock.monthLabelRadius : 0 - theme.clock.monthLabelRadius
-		const labelAngle = upsideDown ? midAngle + Math.PI          : midAngle
-
-		theme.clock.drawing.text(0, yOffset, month.name)
-			.addClass("label month")
-			.attr({
-				'transform': svgRotateString(Snap.deg(labelAngle),0,0)
-			})
+		const sectorPath = sector(month.startAngle, month.endAngle, theme.clock.innerRadius, theme.clock.outerRadius );
+		sectorSvg = `<path d="${sectorPath}" class="sector ${month.code}"></path>`;
+		newSvg += sectorSvg;
 	}
-}/* drawMonths */
+	theme.clock.element.innerHTML += `<g class="month sector">${newSvg}</g>`;
+}/* drawMonthSectors */
+
+
+/* drawMonthLabels
+*/
+theme.clock.drawMonthLabels = function() {
+	newSvg = '';
+	for (let month of config.months)
+	{
+		const center     = polarPoint(month.midAngle, theme.clock.monthLabelRadius);
+		const invert    = (Math.cos(month.midAngle) < 0);
+		const rotate    = degrees(month.midAngle) + ((invert) ? 180 : 0);
+		const transform = `rotate(${rotate}, ${center.x}, ${center.y})`;
+
+		const labelSvg =
+			`<text x="${center.x}" y="${center.y}" transform="${transform}">${month.name}</text>`;
+		newSvg += labelSvg;
+	}
+	theme.clock.element.innerHTML += `<g class="month label">${newSvg}</g>`;
+}/* drawMonthLabels */
 
 
 
+/* drawDayTicks
+*/
 theme.clock.drawDayTicks = function() {
-	// Day Ticks
+
+	const weekdayTickInnerRadius = theme.clock.outerRadius - theme.clock.weekdayTickLength;
+	const weekendTickInnerRadius = theme.clock.outerRadius - theme.clock.weekendTickLength
+
+	let newSvg = '';
+
 	for (let day of config.days)
 	{
-		const angle = dateRadians(day.date)
+		const angle = dateRadians(day.date);
 
-		if (day.first) // If first day in month
+		if (day.first) // Draw long line
 		{
-			// Draw First Tick
-			snapRadialLine(theme.clock.drawing, angle, theme.clock.innerRadius, theme.clock.outerRadius)
-				.addClass("tick day first")
-		}
-
-		if (!day.weekend && !day.first) // If neither weekend nor first day in month
-		{
-			// Draw a standard day tick
-			const tickInnerRadius = theme.clock.outerRadius -theme.clock.weekdayTickLength
-
-			snapRadialLine(theme.clock.drawing, angle, tickInnerRadius, theme.clock.outerRadius)
-				.addClass("tick day weekday")
+			const first = radialLine(angle, theme.clock.outerRadius, theme.clock.innerRadius);
+			const firstSvg =
+				`<line data-date="${day.isoShort}" class="first" x1="${first.xStart}" y1="${first.yStart}" x2="${first.xEnd}" y2="${first.yEnd}" ></line>`;
+			newSvg += firstSvg;
 		}
 
 		if (day.weekend)
 		{
-			// Draw a weekend tick
-			const tickInnerRadius = theme.clock.outerRadius -theme.clock.weekendTickLength
-
-			snapRadialLine(theme.clock.drawing, angle, tickInnerRadius, theme.clock.outerRadius)
-				.addClass("tick day weekend")
+			const weekend = radialLine(angle, theme.clock.outerRadius, weekendTickInnerRadius);
+			const weekendSvg =
+				`<line data-date="${day.isoShort}" class="weekend" x1="${weekend.xStart}" y1="${weekend.yStart}" x2="${weekend.xEnd}" y2="${weekend.yEnd}"></line>`;
+			newSvg += weekendSvg;
+		}
+		else // If neither weekend nor first day in month
+		{
+			const weekday = radialLine(angle, theme.clock.outerRadius, weekdayTickInnerRadius);
+			const weekdaySvg =
+				`<line data-date="${day.isoShort}" class="weekday" x1="${weekday.xStart}" y1="${weekday.yStart}" x2="${weekday.xEnd}" y2="${weekday.yEnd}"></line>`;
+			newSvg += weekdaySvg;
 		}
 	}
-}/* dayTicks */
+	theme.clock.element.innerHTML += `<g class="day tick">${newSvg}</g>`;
+}/* drawDayTicks */
 
 
 
@@ -107,16 +117,17 @@ theme.clock.drawYear = function() {
 	// Year Label
 	const yearOnLeft = dateRatio(config.date) < 0.5
 	const labelSide = yearOnLeft ? -1 : 1
+	const x = theme.clock.innerRadius * 0.55 * labelSide;
+	const y = 0;
 
-	theme.clock.drawing.text(theme.clock.innerRadius * 0.55 * labelSide, 0, config.year)
-		.addClass("label year")
+	const svg = `<text x="${x}" y="${y}" class="label year">${config.year}</text>`;
+
+	theme.clock.element.innerHTML += svg;
 }
 
 
-
 theme.clock.drawNeedle = function() {
-	// Needle
-	const needlePathString = `
+	const path = `
 		M 12 160
 		L -12 160
 		L 0, -${theme.clock.needleLength}
@@ -124,10 +135,10 @@ theme.clock.drawNeedle = function() {
 		M 30 0
 		A 30,30 0 1 1 -30,00
 		A 30,30 0 1 1 30,00`;
-
-	const needleTransformString = svgRotateString(dateDegrees(config.date),0,0);
-
-	theme.clock.drawing.path(needlePathString)
-		.transform(needleTransformString)
-		.addClass("needle year");
+	const transform = svgRotateString(dateDegrees(config.date),0,0);
+	const svg = `
+		<g class="needle">
+			<path d="${path}" transform="${transform}"></path>
+		</g>`;
+	theme.clock.element.innerHTML += svg;
 }
