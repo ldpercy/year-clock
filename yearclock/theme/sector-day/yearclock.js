ccb6b959867b00dff8426da19ca470a34e0fca39
Paul Dale <ldpercy@gmail.com>
2025-04-14 21:27:07 +1000

    Add drawMonthDaySectors

--- yearclock/theme/sector-day/yearclock.js
+++ yearclock/theme/sector-day/yearclock.js
@@ -45,3 +45,66 @@ theme.clock.drawClock = function()
 
 
 function formatMonth(name) { return name.slice(0,3) }
+
+
+
+
+
+
+
+
+/* drawMonthDaySectors
+*/
+theme.clock.drawMonthDaySectors = function() {
+
+	const monthDayTicks = theme.clock.getPeriodDayTicks(config.monthDayArray);
+	theme.clock.element.innerHTML += `
+		<g class="day monthDay sector">
+			${monthDayTicks}
+		</g>`;
+}/* drawMonthDaySectors */
+
+
+/* getPeriodDaySectors
+*/
+theme.clock.getPeriodDaySectors = function(periodArray) {
+
+	const weekdayTickInnerRadius = theme.clock.outerRadius - theme.clock.weekdayMarkerLength;
+	const weekendTickInnerRadius = theme.clock.outerRadius - theme.clock.weekendMarkerLength
+
+	let result = '';
+	let markerClass = '';
+	let markerLine;
+	let markerSvg = '';
+	let sectorPath = '';
+
+	for (let day of periodArray)
+	{
+		let thisDivisionRadians = divisionRadians(periodArray.length, day.dayOfPeriod);
+
+		if (day.isWeekend)
+		{
+			tickClass = 'weekend';
+		}
+		else // day.isWeekday
+		{
+			tickClass = 'weekday';
+		}
+
+		if (day.isFirst) // Draw an extra line for firsts of the month
+		{
+			tickClass += ' first';
+		}
+
+		const sectorPath = sector(thisDivisionRadians.radiansStart, thisDivisionRadians.radiansEnd, theme.clock.innerRadius, theme.clock.outerRadius );
+		sectorSvg = `<path d="${sectorPath}" class="sector ${month.code}"></path>`;
+
+		tickSvg =
+			`<line class="${tickClass}" data-number="${day.number}" data-date="${day.isoShort}" x1="${tickLine.xStart}" y1="${tickLine.yStart}" x2="${tickLine.xEnd}" y2="${tickLine.yEnd}" ></line>`;
+		result += tickSvg;
+	}
+
+	return result;
+}/* getPeriodDaySectors */
+
+
