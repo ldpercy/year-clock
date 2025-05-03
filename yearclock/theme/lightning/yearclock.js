1f27f46df0555d81b9231de3f1dd7ab5d06c7a9e
Paul Dale <ldpercy@gmail.com>
2025-05-03 12:47:31 +1000

    Add week ring

--- yearclock/theme/lightning/yearclock.js
+++ yearclock/theme/lightning/yearclock.js
@@ -8,20 +8,20 @@ theme.clock.clockRadius        = 1250;
 
 
 theme.clock.quarterRadiusStart = 0;
-theme.clock.quarterRadiusEnd   = 450;
-theme.clock.monthRadiusStart  = 350;
-theme.clock.monthRadiusEnd    = 850;
-theme.clock.dayRadiusStart    = 750;
+theme.clock.quarterRadiusEnd   = 300;
+theme.clock.monthRadiusStart  = 300;
+theme.clock.monthRadiusEnd    = 600;
+theme.clock.weekRadiusStart   = 600;
+theme.clock.weekRadiusEnd     = 900;
+theme.clock.dayRadiusStart    = 900;
 theme.clock.dayRadiusEnd      = 1200;
 
 
-theme.clock.dateLabelRadius     = 575;
-theme.clock.dateLabelPosition   = new Point(10, theme.clock.dateLabelRadius);  // tiny tweak to horizontal position here, having trouble centering it properly
-theme.clock.yearLabelPosition   = new Point(0, -theme.clock.dateLabelRadius);
+theme.clock.yearLabelPosition   = new Point(0, 0);
 
 
 
-theme.clock.monthLabelRadius  = 1000;
+theme.clock.monthLabelRadius  = 450;
 theme.clock.monthLabel = {};
 theme.clock.monthLabel.sectorPosition = 0.5;
 theme.clock.monthLabel.rotate = false;
@@ -35,7 +35,21 @@ theme.clock.monthHandLength    = 850;
 // formatting functions
 //
 function formatMonth(name) { return name.slice(0,3) }
-function formatDateLabel(date) { return `${(date.getMonth()+1).toString().padStart(2,'0')}-${date.getDate().toString().padStart(2,'0')}` }
+function formatDateLabel(date) {
+	return `${(date.getMonth()+1).toString().padStart(2,'0')}-${date.getDate().toString().padStart(2,'0')}`
+}
+
+function formatSector(sectorType, sector) {
+	let result;
+	log(sector);
+	switch(sectorType) {
+		case 'quarter': result = `${sector.name}`; break;
+		case 'week'   : result = `Week ${sector.name}`; break;
+		default       : result = sector.name; break;
+	}
+
+	return result;
+}
 
 
 
@@ -47,15 +61,17 @@ theme.clock.drawClock = function(clockElement)
 	theme.clock.element = clockElement;
 	theme.clock.drawFace();
 
-	theme.clock.drawSectors('quarter', getQuarterArray(config.date.object), theme.clock.quarterRadiusStart, theme.clock.quarterRadiusEnd);
+	let quarterArray = getQuarterArray(config.date.object);
+	let weekArray    = getYearWeekArray(config.date.object);
 
+	theme.clock.drawSectors('quarter', quarterArray, theme.clock.quarterRadiusStart, theme.clock.quarterRadiusEnd);
 	theme.clock.drawMonthSectors(theme.clock.monthRadiusStart, theme.clock.monthRadiusEnd);
-
+	theme.clock.drawSectors('week', weekArray, theme.clock.weekRadiusStart, theme.clock.weekRadiusEnd);
 	theme.clock.drawPeriodDaySectors('yearDays', config.yearDayArray, theme.clock.dayRadiusStart, theme.clock.dayRadiusEnd);
 
 	theme.clock.drawMonthLabels();
 	theme.clock.drawYearLabel(config.date.object, theme.clock.yearLabelPosition);
-	theme.clock.drawDateLabel(config.date.object);
+	//theme.clock.drawDateLabel(config.date.object);
 
 }/* drawClock */
 
