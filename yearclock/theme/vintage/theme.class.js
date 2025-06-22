916817723490f73f82b5176960a4c2bfea00b646
Paul Dale <ldpercy@gmail.com>
2025-06-23 00:08:39 +1000

    Gradients and backgrounds

--- yearclock/theme/vintage/theme.class.js
+++ yearclock/theme/vintage/theme.class.js
@@ -35,7 +35,23 @@ themeClass['vintage'] = class extends ThemeBase {
 		displayDate.monthDayArray = getPeriodDayArray(startOfMonth(displayDate.object), nextMonth(displayDate.object), displayDate.object, displayDate.language);
 
 		const clockSVG = `
-			<svg id="clock" class="yearclock" viewBox="${this.viewBox}" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg">
+			<svg
+				id="clock"
+				class="yearclock"
+				viewBox="${this.viewBox}"
+				preserveAspectRatio="xMidYMid meet"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="url(#Gradient1)"
+				>
+
+				<!--
+				<rect x="-1200" y="-1200" width="2400" height="2400">
+				</rect>
+				-->
+
+
+
+				${this.getDefs()}
 				${this.getFace(this.clockRadius)}
 				${this.getMonthSectors(displayDate.monthArray, this.outerRadius, this.innerRadius)}
 				${this.getPeriodDaySectors('month', displayDate.monthDayArray, this.innerRadius, this.outerRadius)}
@@ -59,6 +75,27 @@ themeClass['vintage'] = class extends ThemeBase {
 		}
 		return result;
 	}
+
+
+	/* getDefs
+	*/
+	getDefs = function() {
+		const result = `
+			<defs>
+				<linearGradient id="Gradient1">
+					<stop class="stop1" offset="0%" />
+					<stop class="stop2" offset="50%" />
+					<stop class="stop3" offset="100%" />
+				</linearGradient>
+				<linearGradient id="Gradient2" x1="0" x2="0" y1="0" y2="1">
+					<stop offset="0%" stop-color="red" />
+					<stop offset="50%" stop-color="black" stop-opacity="0" />
+					<stop offset="100%" stop-color="blue" />
+				</linearGradient>
+			</defs>
+		`;
+		return result;
+	}/* getDefs */
 
 
 }/* vintage */
