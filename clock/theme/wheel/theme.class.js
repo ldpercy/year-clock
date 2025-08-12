be1c3d03ab9dea6091a059eae65cadf20c047be2
Paul Dale <ldpercy@gmail.com>
2025-08-12 18:32:47 +1000

    Knockout take 2 - still not working

--- clock/theme/wheel/theme.class.js
+++ clock/theme/wheel/theme.class.js
@@ -8,8 +8,15 @@ themeClass['wheel'] = class extends ThemeBase {
 	// innerRadius       = 950;
 
 	monthSector = {
-		outerRadius       : 1150,
-		innerRadius       : 950,
+		radiusStart       : 1150,
+		radiusEnd         : 950,
+
+		label : {
+			radius         : 1075,
+			sectorPosition : 0.5,
+			rotate         : true,
+			invert         : false,
+		}
 	};
 
 	daySector = {
@@ -18,12 +25,7 @@ themeClass['wheel'] = class extends ThemeBase {
 	};
 
 
-	monthLabel = {
-		radius         : 1075,
-		sectorPosition : 0.5,
-		rotate         : true,
-		invert         : false,
-	}
+
 
 
 	dayName = {
@@ -66,29 +68,26 @@ themeClass['wheel'] = class extends ThemeBase {
 		const monthDayDivision = divisionDegrees(displayDate.daysInMonth, displayDate.date-1);
 		const monthTransform = `rotate(${-monthDayDivision.middle},0,0)`;
 
+		// ${this.getSectors('month', displayDate.monthArray, this.monthSector.outerRadius, this.monthSector.innerRadius)}
+
 		const themeSVG = `
 
 			<defs>
-				<clipPath id="sectorLabels">
-					${this.getSectorLabelsCurved('month', displayDate.monthArray, this.monthLabel)}
-				</clipPath>
 
 			</defs>
 
+			${this.getFace(this.clockRadius)}
 
-			<g transform="${yearTransform}">
-				${this.getFace(this.clockRadius)}
-				${this.getSectors('month', displayDate.monthArray, this.monthSector.outerRadius, this.monthSector.innerRadius)}
+			<!-- <g transform="${yearTransform}"> -->
+				${this.getSectorsWithKnockout('month', displayDate.monthArray, this.monthSector)}
+			<!-- </g> -->
 
-				${this.getSectorLabelsCurved('monthName', displayDate.monthArray, this.monthLabel)}
-
-			</g>
-
+			<!--
 			<g transform="${monthTransform}">
 				${this.getPeriodDaySectors('day', displayDate.monthDayArray, this.daySector.innerRadius, this.daySector.outerRadius)}
 				${this.getSectorLabels('dayNumber', displayDate.monthDayArray, this.dayName)}
 			</g>
-
+			-->
 
 			${this.getDateLabel('year', displayDate, this.dateLabel)}
 		`;
@@ -98,10 +97,87 @@ themeClass['wheel'] = class extends ThemeBase {
 
 
 
-	getMonthSectors = function() {
+	/* getSectorsWithKnockout
+		labelSetting = {
+			radius         : number,
+			invert         : boolean,
+		};
+	*/
+	getSectorsWithKnockout = function(sectorType, sectorArray, settings)
+	{
+		//log('getSectorsKnockout:', arguments);
+
+		/*
+		need sector and label information
+		draw sectors normally
+		draw and extra set of sectors with labels to use as masks
+		will have to create a set of mask ids to dynamically apply to the actual drawn sectors
+		*/
+
+		let sectorMasks = '';
+		let labelPaths = '';
+		let labelArc = '';
+		let sectors = '';
 
 
-	}/* getMonthSectors */
+		for (let sector of sectorArray)
+		{
+			//log('sector:', sector);
+
+			const pathId = `labelPath-${sectorType}-${sector.id}`;
+			const maskId = `sectorMask-${sectorType}-${sector.id}`;
+
+			// label paths:
+			if (settings.label.invert && (Math.cos(sector.radians.middle) < 0)) {
+				labelArc = getArcPath(sector.radians.end, sector.radians.start, settings.label.radius);
+			}
+			else {
+				labelArc = getArcPath(sector.radians.start, sector.radians.end, settings.label.radius);
+			}
+
+			const labelPath = `<path id="${pathId}" d="${labelArc}"/>`;
+			labelPaths += labelPath;
+
+			// textPath:
+			const textPath = `<textPath class="${sector.class}" startOffset="50%" href="#${pathId}">${this.formatLabel(sectorType, sector)}</textPath>`;
+
+			//const text = `<text class="${sector.class}">${this.formatLabel(sectorType, sector)}</text>`;
+
+
+			// sector path, mask, sector itself:
+			const sectorPath = getSectorPath(sector.radians.start, sector.radians.end, settings.radiusStart, settings.radiusEnd);
+
+			const sectorMask = `
+				<mask id="${maskId}" class="sectorMask-${sectorType}">
+					<path d="${sectorPath}"/>
+					${textPath}
+				</mask>
+			`;
+			sectorMasks += sectorMask;
+
+			const sectorSVG =
+				`<path
+					d="${sectorPath}"
+					class="sector ${sectorType}-${sector.id} ${sector.class}"
+					style="mask: url(#${maskId});"
+					>
+					<title>${this.formatTitle(sectorType, sector)}</title>
+				</path>`;
+			sectors += sectorSVG;
+		}
+
+		const result =
+			`<g class="sector ${sectorType}">
+				<defs>
+					${labelPaths}
+					${sectorMasks}
+				</defs>
+				${sectors}
+			</g>`;
+		return result;
+
+	}/* getSectorsWithKnockout */
+
 
 
 }/* Plain SVG */
