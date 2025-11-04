afb5f50b9aa815dfe08d0223652f549407463f60
Paul Dale <ldpercy@gmail.com>
2025-11-04 15:36:08 +1100

    Stringify test results

--- yearclock/yearClockApp.js
+++ yearclock/yearClockApp.js
@@ -359,7 +359,7 @@ yearclock.App = class extends ldpercy.HTMLApp {
 		else {
 			document.getElementById('clockContainer').classList.remove('testPass');
 			document.getElementById('clockContainer').classList.add('testFail');
-			console.error(result);
+			console.error('runTest:', JSON.stringify(result));
 		}
 		document.getElementById('testResult').innerHTML = JSON.stringify(result);
 	}/* runTest */
