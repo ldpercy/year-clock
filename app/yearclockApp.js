59fb803d3e82b8fb9a4ed11fe32592c02cf0ee1b
Paul Dale <ldpercy@gmail.com>
2026-04-29 14:44:13 +1000

    Set branch emojis

--- app/yearclockApp.js
+++ app/yearclockApp.js
@@ -13,7 +13,7 @@ import { clockView } from './view-clock.js';
 
 class YearclockApp extends HTMLApp {
 
-	appVersion = 'v2.2.0';
+	appVersion = 'v2.🖧👚';
 	projectColour = 'gold';
 	appInfo = [`%c
 		Year Clock ${this.appVersion} by ldpercy
